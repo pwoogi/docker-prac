f5244cf286ddc403d0e7bd6398e209cf08c3b16f
pwoogi <chrishw1029@gmail.com>
2023-01-23 16:55:57 +0900

    first commit

---
+++ src/main/java/com/example/dockerkakao/DockerKakaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerkakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerKakaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerKakaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockerkakao/DockerKakaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerkakao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerKakaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
